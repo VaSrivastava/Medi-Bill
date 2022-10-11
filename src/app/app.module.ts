1ca967685870c5aab8907189391ae66bebeba623
VaSrivastava <varun.srivastava@triconit.com>
2022-10-11 17:44:34 +0530

    intial web layout structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderModule } from '../shared/header/header.module'
+import { HeaderModule } from '../shared/header/header.module';
+import { FooterModule } from '../shared/footer/footer.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HeaderModule
+    BrowserModule, HeaderModule, FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
